f562516a6a328b8b2c5df0b9dc46b4081caffb5f
mohamed ashraf <mohamed.ashraf65@msa.edu.eg>
2025-12-14 21:54:15 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,7 @@ module "eks" {
 
 
 resource "aws_security_group" "bastion_sg" {
-  name        = bastion_sg"
+  name        = "bastion_sg"
   vpc_id      = module.vpc.vpc_id
   description = "Allow traffic only from Bastion"
 
