28167e9b8937960409d5a985a0380e5350e6a8bf
mohamed ashraf <mohamed.ashraf65@msa.edu.eg>
2025-12-15 22:27:50 +0200

    Update subnets.tf

--- terraform/vpc/subnets.tf
+++ terraform/vpc/subnets.tf
@@ -13,14 +13,17 @@ resource "aws_subnet" "private_subnets" {
 
 
 resource "aws_subnet" "public_subnets" {
-  count= length(var.public_subnet_cidrs)
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.public_subnet_cidrs[count.index]
-  availability_zone = var.availability_zones[count.index]
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
 
-  tags={
-    Name="${var.cluster_name}-public-subnet-${count.index+1}"
-    "kubernetes.io/cluster/${var.cluster_name}"         =   "shared"
-    "kubernetes.io/role/elb"                            =   "1"
+  tags = {
+    Name = "${var.cluster_name}-public-subnet-${count.index + 1}"
+
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
   }
 }
+
