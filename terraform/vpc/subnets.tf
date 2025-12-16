34a1668d9a6610730c4eafc69652c7afbdef1387
mohamed ashraf <mohamed.ashraf65@msa.edu.eg>
2025-12-16 14:18:39 +0200

    Update subnets.tf

--- terraform/vpc/subnets.tf
+++ terraform/vpc/subnets.tf
@@ -17,7 +17,7 @@ resource "aws_subnet" "public_subnets" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidrs[count.index]
   availability_zone       = var.availability_zones[count.index]
-  map_public_ip_on_launch = true
+
 
   tags = {
     Name = "${var.cluster_name}-public-subnet-${count.index + 1}"
